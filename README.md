5458ddd09791a7c83b641d6b64c8a73665a37371
허성진 <tjdwls75666@naver.com>
2025-02-14 10:41:58 +0900

    react 초기 세팅 추가

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 대덕인제 개발원 스터디
+
+
+## study_1 실행 방법
+
+```
+  // npm 패키지 실행
+  npm run p1_install
+  // fe프로젝트 실행
+  npm run dev
+```
---
+++ React_SpringBoot/fe/src/App.jsx
@@ -0,0 +1,11 @@
+import Board from "./board/Board";
+
+function App() {
+  return (
+    <>
+      <Board />
+    </>
+  );
+}
+
+export default App;
