cd9dd70df515dc40c281717896be8eaaec054171
허성진 <tjdwls75666@naver.com>
2025-02-19 23:33:20 +0900

    ss

--- React_SpringBoot/be/src/main/java/com/example/study/react_springboot/BeApplication.java
+++ React_SpringBoot/be/src/main/java/com/example/study/react_springboot/BeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BeApplication.class, args);
+        SpringApplication.run(BeApplication.class, args); 
 
         
     }
